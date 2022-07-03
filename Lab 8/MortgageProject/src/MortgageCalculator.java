8f72a5c4ebc4712171fbb06df953e2fd0f458e1c
Luzan Baral <luzanbaral@gmail.com>
2022-07-03 18:03:50 -0500

    code cleanup for mortgage calculator

--- Lab 8/MortgageProject/src/MortgageCalculator.java
+++ Lab 8/MortgageProject/src/MortgageCalculator.java
@@ -2,58 +2,58 @@ import java.time.LocalDate;
 import java.time.Period;
 
 public class MortgageCalculator {
+
+	public boolean isProfessionDeveloperOrArchitectOrScrumMaster(String profession) {
+		if (profession.equals("Developer") || profession.equals("Architect") || profession.equals("Scrum master")) return true;
+		return false;
+	}
 	
-	public double computeMaxMortgage(int yearOfBirt, int month, int day, double monthlyIncome, boolean married, double monthlyIncomePartner, String profession) {
+	public boolean isProfessionTesterOrSystemAdministratorOrTechnicalWriter(String profession) {
+		if (profession.equals("Tester") || profession.equals("System Administrator") || profession.equals("Technical writer")) return true;
+		return false;
+	}
+
+	public boolean isProfessionDepartmentHeadOrProfessor(String profession) {
+		if (profession.equals("Department head") || profession.equals("Professor") ) return true;
+		return false;
+	}
+
+	public double computeMortgageByProfessionAndIncome(String profession, double income) {
+		double result = 0;
+		if (2000<=income && income<3000) {
+			if (isProfessionDeveloperOrArchitectOrScrumMaster(profession)) result = 160000;
+			if (isProfessionTesterOrSystemAdministratorOrTechnicalWriter(profession)) result = 120000;
+			if (isProfessionDepartmentHeadOrProfessor(profession)) result = 220000;
+		}
+		if (3000<=income && income<5000) {
+			if (isProfessionDeveloperOrArchitectOrScrumMaster(profession)) result = 180000;
+			if (isProfessionTesterOrSystemAdministratorOrTechnicalWriter(profession)) result = 140000;
+			if (isProfessionDepartmentHeadOrProfessor(profession)) result = 250000;
+		}
+		if (5000<=income) {
+			if (isProfessionDeveloperOrArchitectOrScrumMaster(profession)) result = 220000;
+			if (isProfessionTesterOrSystemAdministratorOrTechnicalWriter(profession)) result = 160000;
+			if (isProfessionDepartmentHeadOrProfessor(profession)) result = 280000;
+		}
+		return result;
+	}
+	
+	public double computeMaxMortgage(int yearOfBirth, int month, int day, double monthlyIncome, boolean married, double monthlyIncomePartner, String profession) {
 		double result=0;
-		//calculate age
-		LocalDate today = LocalDate.now();                          //Today's date
-		LocalDate birthday = LocalDate.of(yearOfBirt, month, day);  //Birth date
+		LocalDate today = LocalDate.now();
+		LocalDate birthday = LocalDate.of(yearOfBirth, month, day);
 		 
 		Period p = Period.between(birthday, today);
 		int age = p.getYears();
-		System.out.println(age);
 
-		if (! married) {
-			if (age >18 && 2000<=monthlyIncome && monthlyIncome<3000) {
-				if (profession.equals("Developer") || profession.equals("Architect") || profession.equals("Scrum master")) result = 160000;
-				if (profession.equals("Tester") || profession.equals("System Administrator") || profession.equals("Technical writer")) result = 120000;
-				if (profession.equals("Department head") || profession.equals("Professor") ) result = 220000;
-			}
-			if (age >18 && 3000<=monthlyIncome && monthlyIncome<5000) {
-				if (profession.equals("Developer") || profession.equals("Architect") || profession.equals("Scrum master")) result = 180000;
-				if (profession.equals("Tester") || profession.equals("System Administrator") || profession.equals("Technical writer")) result = 140000;
-				if (profession.equals("Department head") || profession.equals("Professor") ) result = 250000;
-			}
-			if (age >18 && 5000<=monthlyIncome) {
-				if (profession.equals("Developer") || profession.equals("Architect") || profession.equals("Scrum master")) result = 220000;
-				if (profession.equals("Tester") || profession.equals("System Administrator") || profession.equals("Technical writer")) result = 160000;
-				if (profession.equals("Department head") || profession.equals("Professor") ) result = 280000;
-			}
-			if (age < 18) {
-				result = 0;
-			}
-		}
-		else {
+		if (age < 18) return 0;
+
+		if (!married) {
+			result = computeMortgageByProfessionAndIncome(profession, monthlyIncome);
+			
+		} else {
 			double totalIncome = monthlyIncome + monthlyIncomePartner * 0.94; 
-			if (age >18 && 2000<=totalIncome && totalIncome<3000) {
-				if (profession.equals("Developer") || profession.equals("Architect") || profession.equals("Scrum master")) result = 160000;
-				if (profession.equals("Tester") || profession.equals("System Administrator") || profession.equals("Technical writer")) result = 120000;
-				if (profession.equals("Department head") || profession.equals("Professor") ) result = 220000;
-			}
-			if (age >18 && 3000<=totalIncome && totalIncome<5000) {
-				if (profession.equals("Developer") || profession.equals("Architect") || profession.equals("Scrum master")) result = 180000;
-				if (profession.equals("Tester") || profession.equals("System Administrator") || profession.equals("Technical writer")) result = 140000;
-				if (profession.equals("Department head") || profession.equals("Professor") ) result = 250000;
-			}
-			if (age >18 && 5000<=totalIncome) {
-				if (profession.equals("Developer") || profession.equals("Architect") || profession.equals("Scrum master")) result = 220000;
-				if (profession.equals("Tester") || profession.equals("System Administrator") || profession.equals("Technical writer")) result = 160000;
-				if (profession.equals("Department head") || profession.equals("Professor") ) result = 280000;
-			}
-			if (age < 18) {
-				result = 0;
-			}
-
+			result = computeMortgageByProfessionAndIncome(profession, totalIncome);
 		}
 		
 		return result;
